4bbce0da8c500650174c8cae3cd0cdc67b065306
gourav mahobe <gauravmahobe.mahobe@gmail.com>
2025-01-25 17:37:41 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,36 @@ import userRoutes from "./routes/user.route";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
+
+const allowedOrigins = [
+  process.env.CLIENT_URL_LOCAL, // Local frontend for testing
+  process.env.CLIENT_URL_LIVE, // Replace with your production frontend URL
+];
+
+
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); 
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
   })
 );
+app.use(express.json());
+app.use(cookieParser());
 
 //Health check endpoint at root
 app.get("/api", (req, res) => {
   res.status(200).send("Server is live");
 });
 
-app.use(express.json());
-app.use(cookieParser());
+
 
 //import routes
 import { 
